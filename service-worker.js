0da18611bb3dc9d0904f72a46ff31e93e781939f
baripembo <erikawei@gmail.com>
2018-11-12 23:33:19 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/viz-needs-response/precache-manifest.6c1aa2e178465f6d9efc7ea34f5b0c99.js"
+  "/viz-needs-response/precache-manifest.9cd73ad00a865b84fd25516debf9be71.js"
 );
 
 workbox.clientsClaim();
